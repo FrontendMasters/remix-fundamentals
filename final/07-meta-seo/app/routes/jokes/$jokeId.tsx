0029ddcd3ca15ef71d88df72a6122c996c0a8138
Kent C. Dodds <me+github@kentcdodds.com>
2022-01-02 12:09:11 -0700

    exercise 7

--- final/07-meta-seo/app/routes/jokes/$jokeId.tsx
+++ final/07-meta-seo/app/routes/jokes/$jokeId.tsx
@@ -1,8 +1,25 @@
-import type { ActionFunction, LoaderFunction } from "remix";
+import type { ActionFunction, LoaderFunction, MetaFunction } from "remix";
 import { Link, useLoaderData, useCatch, redirect, useParams } from "remix";
 import type { Joke } from "@prisma/client";
 import { db } from "~/utils/db.server";
 import { getUserId, requireUserId } from "~/utils/session.server";
+
+export const meta: MetaFunction = ({
+  data,
+}: {
+  data: LoaderData | undefined;
+}) => {
+  if (!data) {
+    return {
+      title: "No joke",
+      description: "No joke found",
+    };
+  }
+  return {
+    title: `"${data.joke.name}" joke`,
+    description: `Enjoy the "${data.joke.name}" joke and much more`,
+  };
+};
 
 type LoaderData = { joke: Joke; isOwner: boolean };
 
