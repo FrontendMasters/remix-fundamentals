357345e66522bb2d73b8c74b41915f2ba7c0626d
Kent C. Dodds <me+github@kentcdodds.com>
2022-01-03 22:46:34 -0700

    exercise 9

---
+++ exercise/09-javascript/app/routes/jokes/index.tsx
@@ -0,0 +1,49 @@
+import type { LoaderFunction } from "remix";
+import { useLoaderData, Link, useCatch } from "remix";
+import type { Joke } from "@prisma/client";
+import { db } from "~/utils/db.server";
+
+type LoaderData = { randomJoke: Joke };
+
+export const loader: LoaderFunction = async () => {
+  const count = await db.joke.count();
+  const randomRowNumber = Math.floor(Math.random() * count);
+  const [randomJoke] = await db.joke.findMany({
+    take: 1,
+    skip: randomRowNumber,
+  });
+  if (!randomJoke) {
+    throw new Response("No random joke found", {
+      status: 404,
+    });
+  }
+  const data: LoaderData = { randomJoke };
+  return data;
+};
+
+export default function JokesIndexRoute() {
+  const data = useLoaderData<LoaderData>();
+
+  return (
+    <div>
+      <p>Here's a random joke:</p>
+      <p>{data.randomJoke.content}</p>
+      <Link to={data.randomJoke.id}>"{data.randomJoke.name}" Permalink</Link>
+    </div>
+  );
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+
+  if (caught.status === 404) {
+    return (
+      <div className="error-container">There are no jokes to display.</div>
+    );
+  }
+  throw new Error(`Unexpected caught response with status: ${caught.status}`);
+}
+
+export function ErrorBoundary() {
+  return <div className="error-container">I did a whoopsies.</div>;
+}
---
+++ exercise/09-javascript/app/routes/jokes[.]rss.tsx
@@ -0,0 +1,71 @@
+import type { LoaderFunction } from "remix";
+import { db } from "~/utils/db.server";
+
+function escapeCdata(s: string) {
+  return s.replaceAll("]]>", "]]]]><![CDATA[>");
+}
+
+function escapeHtml(s: string) {
+  return s
+    .replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll('"', "&quot;")
+    .replaceAll("'", "&#039;");
+}
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const jokes = await db.joke.findMany({
+    take: 100,
+    orderBy: { createdAt: "desc" },
+    include: { jokester: { select: { username: true } } },
+  });
+
+  const host =
+    request.headers.get("X-Forwarded-Host") ?? request.headers.get("host");
+  if (!host) {
+    throw new Error("Could not determine domain URL.");
+  }
+  const protocol = host.includes("localhost") ? "http" : "https";
+  const domain = `${protocol}://${host}`;
+  const jokesUrl = `${domain}/jokes`;
+
+  const rssString = `
+    <rss xmlns:blogChannel="${jokesUrl}" version="2.0">
+      <channel>
+        <title>Remix Jokes</title>
+        <link>${jokesUrl}</link>
+        <description>Some funny jokes</description>
+        <language>en-us</language>
+        <generator>Kody the Koala</generator>
+        <ttl>40</ttl>
+        ${jokes
+          .map((joke) =>
+            `
+            <item>
+              <title><![CDATA[${escapeCdata(joke.name)}]]></title>
+              <description><![CDATA[A funny joke called ${escapeHtml(
+                joke.name
+              )}]]></description>
+              <author><![CDATA[${escapeCdata(
+                joke.jokester.username
+              )}]]></author>
+              <pubDate>${joke.createdAt.toUTCString()}</pubDate>
+              <link>${jokesUrl}/${joke.id}</link>
+              <guid>${jokesUrl}/${joke.id}</guid>
+            </item>
+          `.trim()
+          )
+          .join("\n")}
+      </channel>
+    </rss>
+  `.trim();
+
+  return new Response(rssString, {
+    headers: {
+      "Cache-Control": `public, max-age=${60 * 10}, s-maxage=${60 * 60 * 24}`,
+      "Content-Type": "application/xml",
+      "Content-Length": String(Buffer.byteLength(rssString)),
+    },
+  });
+};
