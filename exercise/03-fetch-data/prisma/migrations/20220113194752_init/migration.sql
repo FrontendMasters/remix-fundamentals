4f65fc9629bbaca74fc61128f1503bc83158a033
Kent C. Dodds <me+github@kentcdodds.com>
2022-01-13 13:02:02 -0700

    please work

---
+++ exercise/03-fetch-data/prisma/migrations/20220113194752_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Joke" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "name" TEXT NOT NULL,
+    "content" TEXT NOT NULL
+);
