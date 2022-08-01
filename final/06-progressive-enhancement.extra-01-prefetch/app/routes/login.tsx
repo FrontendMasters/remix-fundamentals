a98f464c3a51b6858d8b730544d9605ad3cbe278
Kent C. Dodds <me+github@kentcdodds.com>
2022-08-01 11:53:43 -0600

    swap login/signup buttons

--- final/06-progressive-enhancement.extra-01-prefetch/app/routes/login.tsx
+++ final/06-progressive-enhancement.extra-01-prefetch/app/routes/login.tsx
@@ -187,18 +187,18 @@ export default function LoginPage() {
             <button
               type="submit"
               name="intent"
-              value="signup"
-              className="w-full rounded bg-gray-500  py-2 px-4 text-white hover:bg-gray-600 focus:bg-gray-400"
-            >
-              Sign up
-            </button>
-            <button
-              type="submit"
-              name="intent"
               value="login"
               className="w-full rounded bg-blue-500  py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
             >
               Log in
+            </button>
+            <button
+              type="submit"
+              name="intent"
+              value="signup"
+              className="w-full rounded bg-gray-500  py-2 px-4 text-white hover:bg-gray-600 focus:bg-gray-400"
+            >
+              Sign up
             </button>
           </div>
         </Form>
