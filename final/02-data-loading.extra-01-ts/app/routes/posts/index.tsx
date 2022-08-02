603b00c50541d98796dc7af9c7b389cbf12ce9ef
Kent C. Dodds <me+github@kentcdodds.com>
2022-08-01 21:18:11 -0500

    improve instructions

--- final/02-data-loading.extra-01-ts/app/routes/posts/index.tsx
+++ final/02-data-loading.extra-01-ts/app/routes/posts/index.tsx
@@ -1,6 +1,18 @@
 import { json } from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
-import { getPosts } from "~/models/post.server";
+
+async function getPosts() {
+  return [
+    {
+      slug: "my-first-post",
+      title: "My First Post",
+    },
+    {
+      slug: "90s-mixtape",
+      title: "A Mixtape I Made Just For You",
+    },
+  ];
+}
 
 export const loader = async () => {
   return json({
